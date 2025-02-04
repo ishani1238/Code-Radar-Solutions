3f386b8c96aecccd5b7729716d687b1b44312c9b
ishani1238 <ishani1238.becse24@chitkara.edu.in>
2025-02-04 18:43:48 +0530

    Solution to Traffic Light Simulation

--- Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -5,11 +5,11 @@ int main(){
     if(a == 'R'){
         printf("Stop\n";)
     }else if(a == 'G'){
-        printf("Go\n";)
+        printf("Go\n");
     }else if(a == "Y"){
         printf("Slow Down\n");
     }else{
-        printf(" Invalid input\n");
+        printf("Invalid input\n");
     }
     return 0;
 }
